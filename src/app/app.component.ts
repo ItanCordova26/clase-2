c67d73866f86c3c7ec3172db44f72678322d7f02
ItanCordova26 <itangaelcq@gmail.com>
2024-02-15 09:42:51 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContadorComponent } from './contador/contador.component';
+import './contador/contador.component.css';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Itan Gael Cordova Quiroz';
+  name = '#20978';
+  number = '5°A';
 }
